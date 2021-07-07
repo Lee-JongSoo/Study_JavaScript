e89c818a9e7717efe04e0c08dc9fe3acfaa2f69e
Lee Jong Soo <jongsu2645@gmail.com>
2021-07-07 20:36:19 +0900

    add ErrorHandler

--- ch05/myLocation.js
+++ ch05/myLocation.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
     }
     else{
         alert("Oops, no geolocation support!")
@@ -15,4 +15,19 @@ function displayLocation(position){
 
     var div = document.getElementById("location");
     div.innerHTML = "Your are at Latitude: " + latitude + ", Longitude: " + longitude;
+}
+
+function displayError(){
+    var errotTypes = {
+        0 : "Unkown error",
+        1 : "Permission denied by user",
+        2 : "Position is not available",
+        3 : "Request timed out"
+    };
+    var errorMessage = errorTypes[error.code];
+    if(error.code = 0 || error.code == 2){
+        errorMessage = errorMessage + " " + error.message;
+    }
+    var div = document.getElementById("location");
+    div.innerHTML = errorMessage;
 }
