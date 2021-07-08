cf01d5a6c435bb0104879acedf5d3cebec8471aa
Lee Jong Soo <jongsu2645@gmail.com>
2021-07-08 19:36:20 +0900

    Add marker

---
+++ ch06/myLoc.js
@@ -0,0 +1,62 @@
+window.onload = getMyLocation;
+
+function getMyLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayLocation);    
+    } else {
+        alert("Oops, no geolocation support");
+    }
+}
+
+function displayLocation(position){
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var div = document.getElementById("location");
+    div.innerHTML = "you are at Latitude : " + latitude + ", Longitude : " + longitude;
+
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
+
+    showMap(position.coords);
+}
+
+
+
+function computeDistance(startCoords, destCoords){
+    var startLatRads = degreesToRadians(startCoords.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+    var destLatRads = degreesToRadians(destCoords.latitude);
+    var destLongRads = degreesToRadians(destCoords.longitude);
+
+    var Radius = 6371; //radius of the Earth in km
+    var distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads)+Math.cos(startLatRads)*Math.cos(destLatRads)*Math.cos(startLongRads - destLongRads)) * Radius;
+
+    return distance;
+}
+
+function degreesToRadians(degrees){
+    var radians = (degrees*Math.PI)/180;
+    return radians;
+}
+
+var ourCoords = {
+    latitude: 37.477128,
+    longitude: 126.981734
+}
+
+
+var map;
+function showMap(coords){
+    var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+    
+    
+    var mapOptions = {
+        zoom:  10,
+        center: googleLatAndLong,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var mapDiv = document.getElementById("map");
+    map = new google.maps.Map(mapDiv, mapOptions);
+}
